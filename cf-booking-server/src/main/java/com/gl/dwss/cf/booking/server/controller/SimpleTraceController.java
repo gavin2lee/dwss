70c5f418a414177dfbc192327305c3b61246c5be
gavin2lee <gavin2lee@163.com>
2017-02-09 18:10:21 +0800

    book

---
+++ cf-booking-server/src/main/java/com/gl/dwss/cf/booking/server/controller/SimpleTraceController.java
@@ -0,0 +1,8 @@
+package com.gl.dwss.cf.booking.server.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SimpleTraceController {
+
+}
---
+++ cf-booking-server/src/main/java/com/gl/dwss/cf/objects/pojo/SimpleObject.java
@@ -0,0 +1,34 @@
+package com.gl.dwss.cf.objects.pojo;
+
+import java.util.Date;
+
+public class SimpleObject {
+	private Long oid;
+	private String content;
+	private Date createAt;
+
+	public Long getOid() {
+		return oid;
+	}
+
+	public void setOid(Long oid) {
+		this.oid = oid;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public Date getCreateAt() {
+		return createAt;
+	}
+
+	public void setCreateAt(Date createAt) {
+		this.createAt = createAt;
+	}
+
+}
