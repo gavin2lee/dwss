9cdf616ef6dd83fed8e96935bbf327dedd3d7ef7
gavin2lee <gavin2lee@163.com>
2017-02-17 17:13:04 +0800

    sync

--- dir-sync-server/src/main/java/com/gl/dwss/dirsync/server/config/DirSyncServerConfiguration.java
+++ dir-sync-server/src/main/java/com/gl/dwss/dirsync/server/config/DirSyncServerConfiguration.java
@@ -1,8 +1,10 @@
 package com.gl.dwss.dirsync.server.config;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication(scanBasePackages={"com.gl.dwss"})
+@EnableConfigurationProperties({DirSyncServerProperties.class})
 public class DirSyncServerConfiguration {
 
 }
