9aef624c2ffebb837071e8c432b3729f7666276c
gavin2lee <gavin2lee@163.com>
2017-06-25 17:58:23 +0800

    comments

--- monitor/monitor-gather/src/main/java/com/gl/monitor/gather/App.java
+++ monitor/monitor-gather/src/main/java/com/gl/monitor/gather/App.java
@@ -6,6 +6,10 @@ import com.gl.monitor.gather.config.AppConfiguration;
 
 public class App {
 
+	/**
+	 *  mvn spring-boot:run -DremoteServerHost=192.168.0.104 -DremoteServerPort=10080 -DsysId=client-01
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		SpringApplication app = new SpringApplication(AppConfiguration.class);
 		app.setWebEnvironment(false);
