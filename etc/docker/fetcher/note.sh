aebdd69b8cce34fb0596dc9f0ee581a811df8b08
gavin2lee <gavin2lee@163.com>
2017-06-25 19:20:43 +0800

    move

--- etc/docker/fetcher/note.sh
+++
@@ -1,3 +0,0 @@
-#/bin/bash
-sudo docker run --name sample -i -t gavin2lee/fetcher https://tomcat.apache.org/tomcat-7.0-doc/appdev/sample/sample.war
-
--- etc/docker/tomcat7/run-tomcat7.sh
+++
@@ -1,2 +0,0 @@
-#/bin/bash
-sudo docker run --name sample_app --volumes-from sample -d -P gavin2lee/tomcat7
