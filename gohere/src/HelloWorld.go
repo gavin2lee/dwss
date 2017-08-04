d87dc9764dccb9f3d99a37e117f554b4fd529235
gavin2lee <gavin2lee@163.com>
2017-08-04 21:27:35 +0800

    go

---
+++ gohere/src/HelloWorld.go
@@ -0,0 +1,16 @@
+package main;
+
+import(
+  "fmt"
+)
+
+func main(){
+  fmt.Println("Hello World");
+
+  var f1 float32 = 1235678900;
+  var f2 float32 = 20;
+
+  var f3 = f1 + f2;
+
+  fmt.Println(f3);
+}
