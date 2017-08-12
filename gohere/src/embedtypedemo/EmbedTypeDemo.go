91fc8d4aecd0ca16f1d9915b2611acd3bd17e339
gavin2lee <gavin2lee@163.com>
2017-08-12 08:14:10 +0800

    go

--- gohere/src/embedtypedemo/EmbedTypeDemo.go
+++ gohere/src/embedtypedemo/EmbedTypeDemo.go
@@ -4,6 +4,8 @@ import (
   "fmt"
 )
 
+import "reflect"
+
 func main(){
   testEmbedType()
 }
@@ -23,6 +25,26 @@ type admin struct{
   level int64
 }
 
+
+type animal struct{
+  name string
+  id int64
+}
+
+func (a *animal) breathe(){
+  fmt.Printf("%s breathing...\n",a.name)
+}
+
+type mammal struct {
+  *animal
+  age int
+}
+
+type cat struct {
+  *mammal
+  color string
+}
+
 func testEmbedType(){
   fmt.Println("test embed type...")
 
@@ -38,4 +60,24 @@ func testEmbedType(){
   fmt.Println("===========")
 
   admin1.notify()
+
+
+  fmt.Println("====animal ======")
+  animal1 := &animal{"Jim",1000}
+  mammal1 := &mammal{age:10,animal:animal1}
+
+  fmt.Println(mammal1)
+  cat1 := &cat{color:"black",mammal:mammal1}
+
+  fmt.Printf("type of cat:%s\n",reflect.TypeOf(cat1))
+
+  cat1.breathe()
+
+  fmt.Println("===== infomation of cat1 ======")
+
+  fmt.Printf("cat1.name:%s\n",cat1.name)
+  fmt.Println("&cat1:", &cat1)
+  fmt.Println("&cat1.name", &cat1.name)
+  fmt.Println("&cat1.age", &cat1.age)
+  fmt.Println("&cat1.color", &cat1.color)
 }
