7be5089aa1e264356f3d1a9a535483f47f26383a
gavin2lee <gavin2lee@163.com>
2017-08-26 23:20:09 +0800

    sh

--- orderg/src/make-env.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-export GOPATH=$GOPATH:/home/gavin/Dev/GitRepo/dwss/orderg
-export GOBIN=$GOBIN:/home/gavin/Dev/GitRepo/dwss/orderg/bin:.
-echo "GOPATH $GOPATH"
-echo "GOBIN $GOBIN"
-
-#source
--- ordergo/src/make-env.sh
+++ ordergo/src/make-env.sh
@@ -1,7 +1,19 @@
 #!/bin/sh
 
-export GOPATH=$GOPATH:/home/gavin/Dev/GitRepo/dwss/ordergo
-export GOBIN=$GOBIN:/home/gavin/Dev/GitRepo/dwss/ordergo/bin
+if [ -z "$GOPATH" ]; then
+    export GOPATH=/home/gavin/Dev/GitRepo/dwss/ordergo
+    
+    echo "GOBIN $GOBIN"
+else
+    export GOPATH=$GOPATH:/home/gavin/Dev/GitRepo/dwss/ordergo
+fi
+if [ "$GOBIN" = "" ]; then
+    export GOBIN=/home/gavin/Dev/GitRepo/dwss/ordergo/bin
+    echo "GOBIN <$GOBIN>"
+else
+    export GOBIN=$GOBIN:/home/gavin/Dev/GitRepo/dwss/ordergo/bin
+    echo "GOBIN <<$GOBIN>>"
+fi
 echo "GOPATH $GOPATH"
 echo "GOBIN $GOBIN"
 
