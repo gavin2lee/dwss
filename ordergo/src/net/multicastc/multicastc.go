c6b8ff0aed7b9ba9df59e904b960d50cdc423e0c
gavin2lee <gavin2lee@163.com>
2017-08-26 12:30:24 +0800

    multicast

--- ordergo/src/net/multicastc/multicastc.go
+++ ordergo/src/net/multicastc/multicastc.go
@@ -1,10 +1,65 @@
 package main 
 
 import (
-
+    "fmt"
+    "net"
+    
+    "strconv"
+    "time"
+    
+    "common/objutils"
 )
 
 func main() {
+    port := 12356
+    service := ":10002"
+    
+    gaddr := net.UDPAddr{
+        IP: net.IPv4(224, 3, 5, 6),
+        Port:port,
+    }
+    
+    fmt.Println("Send to ", gaddr)
+    
+    var round int64
+	round = 0
+	
+	udpAddr,err := net.ResolveUDPAddr("udp", service)
+    objutils.CheckError(err)
+    
+    udpConn,err := net.ListenUDP("udp", udpAddr)
+    objutils.CheckError(err)
+    
+    
 
+	conn, err := net.DialUDP("udp", nil, &gaddr)
+
+	objutils.CheckError(err)
+	
+	fmt.Println(udpAddr.String())
+	
+	for {
+		round = round + 1
+		_,err = conn.Write([]byte(udpAddr.String() + "|" + strconv.FormatInt(round, 10)))
+		objutils.CheckError(err)
+		
+		fmt.Println("SEND ", round)
+
+
+		var buf [512]byte
+		
+		
+
+        udpConn.SetReadDeadline(time.Now().Add(3*time.Second))
+		n, addr, err := udpConn.ReadFromUDP(buf[0:])
+		if err != nil {
+		    fmt.Println(err)
+		}
+
+		fmt.Println("Reply from server ", addr.String(), string(buf[0:n]))
+
+		
+		time.Sleep(1*time.Second)
+	}
 }
 
---
+++ ordergo/src/net/multicasts/multicasts.go
@@ -0,0 +1,60 @@
+package main 
+
+import (
+    "fmt"
+    "net"
+    
+    "common/objutils"
+)
+
+func main() {
+    port := 12356
+    
+    gaddr := net.UDPAddr{
+        IP: net.IPv4(224, 3, 5, 6),
+        Port:port,
+    }
+    
+    fmt.Println("Listen ", gaddr)
+    conn,err := net.ListenMulticastUDP("udp", nil, &gaddr)
+    
+    objutils.CheckError(err)
+    
+    for{
+        handleUdpClient(conn)
+    }
+}
+
+func handleUdpClient(conn * net.UDPConn){
+    var buf [512]byte
+    n,addr,err := conn.ReadFromUDP(buf[0:])
+    
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+    
+    fmt.Println("Recv from client ", addr.String(), string(buf[0:n]))
+    n,err = conn.WriteToUDP([]byte("Welcome Client"), addr)
+    
+    fmt.Println("State:", n, "-",err)
+    
+    raddr := net.UDPAddr{
+        IP:net.IPv4(192, 168, 0, 103),
+        Port:10002,
+    }
+    
+    udpConn,err := net.DialUDP("udp", nil, &raddr)
+    if err != nil {
+        fmt.Println("dial", err)
+        return
+    }
+    
+    _,err = udpConn.Write([]byte("Welcome client"))
+    if err != nil {
+        fmt.Println("write", err)
+    }
+    udpConn.Close()
+    
+    fmt.Println("resp ")
+}
