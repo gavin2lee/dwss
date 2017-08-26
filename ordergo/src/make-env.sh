7767e692b0e3e87d390e378cd9faa1ba5b42c603
gavin2lee <gavin2lee@163.com>
2017-08-26 22:33:39 +0800

    sh

---
+++ ordergo/src/make-env.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+export GOPATH=$GOPATH:/home/gavin/Dev/GitRepo/dwss/ordergo
+export GOBIN=$GOBIN:/home/gavin/Dev/GitRepo/dwss/ordergo/bin
+echo "GOPATH $GOPATH"
+echo "GOBIN $GOBIN"
+
+#source
