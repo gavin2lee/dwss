3b52e50d8a625e2b5ce393754bdaffc88511b6fb
gavin2lee <gavin2lee@163.com>
2017-10-11 22:08:40 +0800

    dubbo

--- time-client/src/test/java/com/gl/time/client/service/TimeServiceClientTest.java
+++ time-client/src/test/java/com/gl/time/client/service/TimeServiceClientTest.java
@@ -13,22 +13,42 @@ import com.gl.time.client.dto.SimplePrincipal;
 import com.gl.time.client.dto.SimpleTime;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations={"classpath:spring/application-context-reference.xml"})
+@ContextConfiguration(locations = { "classpath:spring/application-context-reference.xml" })
 public class TimeServiceClientTest {
     private static final Logger LOG = LoggerFactory.getLogger(TimeServiceClientTest.class);
     @Autowired
     TimeServiceClient client;
-    
+
     @Test
     public void testGetTime() {
         SimplePrincipal sp = new SimplePrincipal();
-        sp.setUsername("test"+System.currentTimeMillis());
-        sp.setPassword(""+System.currentTimeMillis());
-        
+        sp.setUsername("test" + System.currentTimeMillis());
+        sp.setPassword("" + System.currentTimeMillis());
+
         SimpleTime st = client.getTime(sp);
-        
+
         Assert.assertNotNull(st);
         LOG.info("TEST RET:{}", st);
+    }
+
+    @Test
+    public void testGetTimeInBatch() {
+        for (int i = 0; i < 1000; i++) {
+            LOG.info("===  {}  ===", i);
+            SimplePrincipal sp = new SimplePrincipal();
+            sp.setUsername("test-" + i+"-"+System.currentTimeMillis());
+            sp.setPassword("" + System.currentTimeMillis());
+
+            SimpleTime st = client.getTime(sp);
+
+            Assert.assertNotNull(st);
+            LOG.info("TEST RET:{}", st);
+            
+            try{
+            Thread.sleep(500);
+            }catch(Exception e)
+            {}
+        }
     }
 
 }
